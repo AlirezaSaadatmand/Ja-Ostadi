665f527938353fab26d5941338f9e9781626385e
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-07-26 20:58:03 +0330

    refactor: department model

--- app/models/department.go
+++ app/models/department.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Department struct {
     gorm.Model
-    Name    string   `gorm:"unique;not null"`
-    Courses []Course
+    Name        string
+    Instructors []InstructorDepartment
+    Courses     []Course
 }
