21c7834ff8ecbd84302e55a3ca47224cbf33d1da
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-07-27 19:30:51 +0330

    add: front setup

---
+++ Front/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-500">
+          Tailwind + React + TS
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
