2a5cd06640adf88cad5712b920b2a664f2c8f83a
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-07-27 23:42:06 +0330

    add: adding a structured reponse for all the project

---
+++ Back/utils/response.go
@@ -0,0 +1,24 @@
+package utils
+
+import "github.com/gofiber/fiber/v2"
+
+type APIResponse struct {
+	Status  string      `json:"status"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func Success(c *fiber.Ctx, code int, data interface{}, message string) error {
+	return c.Status(code).JSON(APIResponse{
+		Status:  "success",
+		Message: message,
+		Data:    data,
+	})
+}
+
+func Error(c *fiber.Ctx, code int, message string) error {
+	return c.Status(code).JSON(APIResponse{
+		Status:  "error",
+		Message: message,
+	})
+}
