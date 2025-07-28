321f126c7a8f14125a7b5281a6e38860198500be
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-07-28 21:41:25 +0330

    add: new routes for schedule

--- Back/routes/router.go
+++ Back/routes/router.go
@@ -15,15 +15,18 @@ func Router(app *fiber.App) {
 
 	// Course Routes
 	courseRouter := api.Group("/courses")
-
 	courseRouter.Get("/semester/:semesterID", handlers.GetCoursesBySemester)
 	courseRouter.Get("/semester/:semesterID/department/:departmentID", handlers.GetCoursesBySemesterAndDepartment)
 	// courseRouter.Get("/:id", handlers.GetCourseByID)
 
 	
+	// Department Routes
 	departmentRouter := api.Group("/departments")
-
 	departmentRouter.Get("/", handlers.GetDepartments)
 
+
+	// Schedule Routes
+	scheduleRouter := api.Group("/schedule")
+	scheduleRouter.Get("/get-courses", )
 }
 
