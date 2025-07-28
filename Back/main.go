795b4a06a647a6b4b3c6c6aaa90747f1ead2e3a5
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-07-28 17:08:18 +0330

    add: adding cors to project

--- Back/main.go
+++ Back/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/routes"
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/scripts"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -23,6 +24,11 @@ func main() {
 	}
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Or "http://localhost:5173" or your front URL
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
