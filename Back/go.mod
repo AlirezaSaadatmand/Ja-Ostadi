2aaada18b2a7d45c34eaf25d10a7f2ccc1c43c8b
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-07-28 13:20:15 +0330

    add: remove the logic from handlers

--- Back/go.mod
+++ Back/go.mod
@@ -3,7 +3,6 @@ module github.com/AlirezaSaadatmand/Ja-Ostadi
 go 1.24.2
 
 require (
-	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.52.9
 	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/mysql v1.6.0
@@ -14,9 +13,7 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
--- Back/handlers/course.handler.go
+++ Back/handlers/course.handler.go
@@ -3,8 +3,7 @@ package handlers
 import (
 	"strconv"
 
-	"github.com/AlirezaSaadatmand/Ja-Ostadi/database"
-	"github.com/AlirezaSaadatmand/Ja-Ostadi/models"
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/services"
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/utils"
 	"github.com/gofiber/fiber/v2"
 )
@@ -15,24 +14,14 @@ func GetCoursesBySemester(c *fiber.Ctx) error {
 		return utils.Error(c, fiber.StatusBadRequest, "semesterID is required")
 	}
 
-	id, err := strconv.ParseUint(semesterID, 10, 64)
+	id, err := strconv.Atoi(semesterID)
 	if err != nil {
 		return utils.Error(c, fiber.StatusBadRequest, "semesterID must be a valid number")
 	}
 
-	type CourseMinimal struct {
-		ID   uint   `json:"id"`
-		Name string `json:"name"`
-	}
-
-	var courses []CourseMinimal
-	err = database.DB.
-		Model(&models.Course{}).
-		Select("ID, Name").
-		Where("semester_id = ?", id).
-		Find(&courses).Error
+	courses, err := services.GetCoursesBySemester(id)
 	if err != nil {
-		return utils.Error(c, fiber.StatusInternalServerError, "Error getting data")
+		return utils.Error(c, fiber.StatusBadRequest, err.Error())
 	}
 
 	return utils.Success(c, fiber.StatusOK, courses, "Data fetched successfully")
@@ -45,25 +34,15 @@ func GetCoursesBySemesterAndDepartment(c *fiber.Ctx) error {
 		return utils.Error(c, fiber.StatusBadRequest, "semesterID and departmentID is required")
 	}
 
-	semester, err1 := strconv.ParseUint(semesterID, 10, 64)
-	department, err2 := strconv.ParseUint(departmentID, 10, 64)
+	semester, err1 := strconv.Atoi(semesterID)
+	department, err2 := strconv.Atoi(departmentID)
 	if err1 != nil || err2 != nil {
 		return utils.Error(c, fiber.StatusBadRequest, "semesterID and departmentID must be a valid number")
 	}
 
-	type CourseMinimal struct {
-		ID   uint   `json:"id"`
-		Name string `json:"name"`
-	}
-
-	var courses []CourseMinimal
-	err := database.DB.
-		Model(&models.Course{}).
-		Select("ID, Name").
-		Where("semester_id = ? and Department_id = ?", semester, department).
-		Find(&courses).Error
+	courses, err := services.GetCoursesBySemesterAndDepartment(semester, department)
 	if err != nil {
-		return utils.Error(c, fiber.StatusInternalServerError, "Error getting data")
+		return utils.Error(c, fiber.StatusBadRequest, err.Error())
 	}
 
 	return utils.Success(c, fiber.StatusOK, courses, "Data fetched successfully")
