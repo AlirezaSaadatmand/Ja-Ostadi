0c74df85a96de2e7d18ec07dbde4b430dfb37926
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-07-28 21:42:16 +0330

    add: adding services for schedule

---
+++ Back/services/schedule.service.go
@@ -0,0 +1,53 @@
+package services
+
+import (
+	"errors"
+
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/database"
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/models"
+)
+
+type ScheduleCourse struct {
+	ID           uint   `json:"id"`
+	Name         string `json:"name"`
+	Number       string `json:"number"`
+	Group        string `json:"group"`
+	Units        int    `json:"units"`
+	SemesterID   uint   `json:"semester_id"`
+	DepartmentID uint   `json:"department_id"`
+	InstructorID uint   `json:"instructor_id"`
+}
+
+func GetCoursesSchedule() ([]ScheduleCourse, error) {
+
+	semesterID := 2
+
+	var courses []ScheduleCourse
+	err := database.DB.
+		Model(&models.Course{}).
+		Where("semester_id = ?", semesterID).
+		Find(&courses).Error
+	if err != nil {
+		return nil, errors.New("error getting data")
+	}
+
+	return courses, nil
+}
+
+type ScheduleInstructor struct {
+	Name string `json:"name"`
+}
+
+func GetCourseInstructor(instructorId int) (ScheduleInstructor, error) {
+	var instructor ScheduleInstructor
+
+	err := database.DB.
+		Model(&models.Instructor{}).
+		Where("id = ?", instructorId).
+		Find(&instructor).Error
+	if err != nil {
+		return instructor, errors.New("error getting data")
+	}
+
+	return instructor, nil
+}
