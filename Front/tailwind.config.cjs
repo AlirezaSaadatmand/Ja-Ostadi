1985d44fd9d0f6abfa2e13ca6bd93a889dffe3cd
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-08-02 20:01:24 +0330

    using the older version of the tailwind

---
+++ Front/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",               // Root HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // All source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
