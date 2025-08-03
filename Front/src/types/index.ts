d9785df16efe7c8bccc19364f05d08859009e320
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-08-03 18:05:50 +0330

    add: new types

--- Front/src/types/index.ts
+++ Front/src/types/index.ts
@@ -1,38 +1,42 @@
-
-
 export interface Course {
-  id: number;
-  name: string;
-  number: string;
-  group: string;
-  units: number;
-  semester_id: number;
-  department_id: number;
-  instructor_id: number;
+  id: number
+  name: string
+  number: string
+  group: string
+  units: number
+  semester_id: number
+  department_id: number
+  instructor_id: number
 }
 
 export interface Instructor {
-  name: string;
+  name: string
 }
 
 export interface CourseTime {
-  day: string;
-  start_time: string;
-  end_time: string;
-  room: string;
+  day: string
+  start_time: string
+  end_time: string
+  room: string
 }
 
 export interface CourseResponse {
-  course: Course;
-  instructor: Instructor;
-  time: CourseTime[];
+  course: Course
+  instructor: Instructor
+  time: CourseTime[]
   department: {
-    name: string;
-  };
+    name: string
+  }
 }
 
-
 export interface Department {
-  id: number;
-  name: string;
+  id: number
+  name: string
+}
+
+export interface DepartmentDetail {
+  id: number
+  name: string
+  instructors_count: number
+  courses_count: number
 }
