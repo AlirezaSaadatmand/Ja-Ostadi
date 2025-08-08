66017ca5d7d9244c0034a369886db9e344872c24
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-08-08 23:48:04 +0330

    add: new store file for handling schedule page

---
+++ Front/src/store/useCourseModalStore.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand"
+import type { CourseResponse } from "../types"
+
+interface CourseModalStore {
+  isOpen: boolean
+  selectedCourse: CourseResponse | null
+  isScheduledCourseInModal: boolean
+  openModal: (course: CourseResponse, isScheduled: boolean) => void
+  closeModal: () => void
+}
+
+export const useCourseModalStore = create<CourseModalStore>((set) => ({
+  isOpen: false,
+  selectedCourse: null,
+  isScheduledCourseInModal: false,
+
+  openModal: (course, isScheduled) =>
+    set({
+      isOpen: true,
+      selectedCourse: course,
+      isScheduledCourseInModal: isScheduled,
+    }),
+  closeModal: () =>
+    set({
+      isOpen: false,
+      selectedCourse: null,
+      isScheduledCourseInModal: false,
+    }),
+}))
