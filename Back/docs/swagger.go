4239256349cdaf7743424fe032d5c1ed1782d783
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-08-16 18:21:41 +0330

    refactor: separating swagger from main file

---
+++ Back/docs/swagger.go
@@ -0,0 +1,27 @@
+package docs
+
+import (
+	"fmt"
+
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/config"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
+)
+
+func SetupSwagger(app *fiber.App) {
+	cfg := config.GetConfig()
+
+	// Dynamic Swagger setup
+	SwaggerInfo.Version = "1.0"
+	SwaggerInfo.Host = fmt.Sprintf("%s:%s", cfg.HOST, cfg.PORT)
+	SwaggerInfo.BasePath = "/api/v1"
+	if cfg.MODE == "production" {
+		SwaggerInfo.Schemes = []string{"https"}
+	} else {
+		SwaggerInfo.Schemes = []string{"http"}
+	}
+	SwaggerInfo.Title = "Ja Ostadi API"
+	SwaggerInfo.Description = "API documentation for Ja Ostadi"
+
+	app.Get("/swagger/*", swagger.HandlerDefault)
+}
--- Back/main.go
+++ Back/main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"fmt"
 
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/config"
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/database"
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/routes"
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/scripts"
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/docs"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-
-	"github.com/AlirezaSaadatmand/Ja-Ostadi/docs"
-	_ "github.com/AlirezaSaadatmand/Ja-Ostadi/docs"
-	"github.com/gofiber/swagger"
 )
 
 func main() {
@@ -30,18 +27,6 @@ func main() {
 		log.Println("✅ Imported successfully")
 	}
 
-	// --- Dynamic Swagger setup ---
-	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = fmt.Sprintf("%s:%s", cfg.HOST, cfg.PORT)
-	docs.SwaggerInfo.BasePath = "/api/v1"
-	if cfg.MODE == "production" {
-		docs.SwaggerInfo.Schemes = []string{"https"}
-	} else {
-		docs.SwaggerInfo.Schemes = []string{"http"}
-	}
-	docs.SwaggerInfo.Title = "Ja Ostadi API"
-	docs.SwaggerInfo.Description = "API documentation for Ja Ostadi"
-
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
@@ -56,8 +41,10 @@ func main() {
 		})
 	})
 
-	app.Get("/swagger/*", swagger.HandlerDefault)
+	// Swagger setup
+	docs.SetupSwagger(app)
 
+	// API routes
 	routes.Router(app)
 
 	if err := app.Listen(fmt.Sprintf(":%s", cfg.PORT)); err != nil {
