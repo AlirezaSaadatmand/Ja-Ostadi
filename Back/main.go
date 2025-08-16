ec5cc68d96dfce5a9e806de2ee0273383f2e9fb1
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-08-16 18:13:34 +0330

    add: adding swagger to project

--- Back/main.go
+++ Back/main.go
@@ -8,12 +8,19 @@ import (
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/database"
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/routes"
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/scripts"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/docs"
+	_ "github.com/AlirezaSaadatmand/Ja-Ostadi/docs"
+	"github.com/gofiber/swagger"
 )
 
 func main() {
+	// Load Config and DB
 	config.LoadConfig()
+	cfg := config.GetConfig()
 	database.ConnectDB()
 
 	err := scripts.ImportData()
@@ -23,22 +30,37 @@ func main() {
 		log.Println("✅ Imported successfully")
 	}
 
+	// --- Dynamic Swagger setup ---
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = fmt.Sprintf("%s:%s", cfg.HOST, cfg.PORT)
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	if cfg.MODE == "production" {
+		docs.SwaggerInfo.Schemes = []string{"https"}
+	} else {
+		docs.SwaggerInfo.Schemes = []string{"http"}
+	}
+	docs.SwaggerInfo.Title = "Ja Ostadi API"
+	docs.SwaggerInfo.Description = "API documentation for Ja Ostadi"
+
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "*", // Or "http://localhost:5173" or your front URL
+		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
 
+	// Root route
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
 			"message": "Welcome to Ja Ostadi API!",
 		})
 	})
 
+	app.Get("/swagger/*", swagger.HandlerDefault)
+
 	routes.Router(app)
 
-	if err := app.Listen(fmt.Sprintf(":%s", config.GetConfig().PORT)); err != nil {
+	if err := app.Listen(fmt.Sprintf(":%s", cfg.PORT)); err != nil {
 		log.Fatal("❌ Failed to start server:", err)
 	}
 }
