f5064e7f5bbadaf0299288f647ff39f84d0d6390
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-08-17 20:41:23 +0330

    refactor: using the service struc and adding logger in semester

--- Back/services/semester.service.go
+++ Back/services/semester.service.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/database"
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/models"
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/pkg/logging"
 )
 
 type SemesterData struct {
@@ -12,30 +13,33 @@ type SemesterData struct {
 	Name string `json:"name"`
 }
 
-func GetSemesters() ([]SemesterData, error) {
-
+func (s *Services) GetSemesters() ([]SemesterData, error) {
 	var semesters []SemesterData
 	err := database.DB.
 		Model(&models.Semester{}).
 		Select("ID, Name").
 		Find(&semesters).Error
 	if err != nil {
+		s.Logger.Error(logging.Mysql, logging.Select, "Failed to fetch semesters", map[logging.ExtraKey]interface{}{"error": err.Error()})
 		return nil, errors.New("error getting data")
 	}
 
+	s.Logger.Info(logging.Mysql, logging.Select, "Fetched all semesters successfully", nil)
 	return semesters, nil
 }
 
-func GetSemesterByID(semeterID int) (SemesterData, error) {
+func (s *Services) GetSemesterByID(semesterID int) (SemesterData, error) {
 	var semester SemesterData
 	err := database.DB.
 		Model(&models.Semester{}).
 		Select("ID, Name").
-		Where("id = ?", semeterID).
+		Where("id = ?", semesterID).
 		Find(&semester).Error
 	if err != nil {
+		s.Logger.Error(logging.Mysql, logging.Select, "Failed to fetch semester by ID", map[logging.ExtraKey]interface{}{"semesterID": semesterID, "error": err.Error()})
 		return semester, errors.New("error getting data")
 	}
 
+	s.Logger.Info(logging.Mysql, logging.Select, "Fetched semester successfully", map[logging.ExtraKey]interface{}{"semesterID": semesterID})
 	return semester, nil
 }
