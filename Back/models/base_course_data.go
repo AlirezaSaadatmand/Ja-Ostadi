48e8c780985ad3175f9ef82e27c93096632f12dc
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-09-08 01:10:23 +0330

    add: new model for saving the whole object of the data

---
+++ Back/models/base_course_data.go
@@ -0,0 +1,21 @@
+package models
+
+import "gorm.io/gorm"
+
+type Base_course_data struct {
+    gorm.Model
+	Semester       string 
+	Department     string 
+	CourseName     string 
+	CourseNumber   string `gorm:"primaryKey"`
+	Group          string 
+	Units          string 
+	ClassType      string 
+	Instructor     string 
+	TimeInWeek     string 
+	TimeRoom       string 
+	MidExamTime    string 
+	FinalExamTime  string 
+	Capacity       string 
+	StudentCount   string
+}
