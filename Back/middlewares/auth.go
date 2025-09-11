43b28ec06cc04c62d9ef8938b1e951e4ff1a8c39
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-09-11 22:39:40 +0330

    add: new middleware for checking authentication

---
+++ Back/middlewares/auth.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/pkg/token"
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Auth() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		authHeader := c.Get("Authorization")
+		if authHeader == "" {
+			return utils.Error(c, fiber.StatusUnauthorized, "Missing Authorization header")
+		}
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			return utils.Error(c, fiber.StatusUnauthorized, "Invalid Authorization header format")
+		}
+
+		tokenStr := parts[1]
+
+		tok, err := token.ParseJWT(tokenStr)
+		if err != nil {
+			return utils.Error(c, fiber.StatusUnauthorized, err.Error())
+		}
+
+		c.Locals("client", tok.Claims)
+		return c.Next()
+	}
+}
