a909970a4dc7cb18ba76094a8fa0f7640c80734a
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-10-11 23:53:28 +0330

    refactor(logger.go): skip the logging of the body of the requests the files

--- Back/middlewares/logger.go
+++ Back/middlewares/logger.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"strings"
 	"time"
 
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/pkg/logging"
@@ -9,32 +10,39 @@ import (
 
 func RequestLogger(logger logging.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-
 		if c.Path() == "/swagger" || c.Path() == "/docs" {
 			return c.Next()
 		}
 
 		start := time.Now()
-		reqBody := c.Body()
+		reqBody := ""
+
+		contentType := c.Get("Content-Type")
+
+		if strings.HasPrefix(contentType, "multipart/form-data") ||
+			strings.Contains(c.Path(), "/image") ||
+			strings.Contains(c.Path(), "/update/data") {
+			reqBody = "[binary data omitted]"
+		} else {
+			reqBody = string(c.Body())
+		}
 
 		err := c.Next()
-
 		latency := time.Since(start)
 
 		keys := map[logging.ExtraKey]interface{}{
-			logging.Path:        c.Path(),
-			logging.Method:      c.Method(),
-			logging.StatusCode:  c.Response().StatusCode(),
-			logging.ClientIp:    c.IP(),
-			logging.Latency:     latency,
-			logging.RequestBody: string(reqBody),
-			logging.BodySize:    len(c.Response().Body()),
-			logging.ErrorMessage: func() string {
-				if err != nil {
-					return err.Error()
-				}
-				return ""
-			}(),
+			logging.Path:         c.Path(),
+			logging.Method:       c.Method(),
+			logging.StatusCode:   c.Response().StatusCode(),
+			logging.ClientIp:     c.IP(),
+			logging.Latency:      latency,
+			logging.RequestBody:  reqBody,
+			logging.BodySize:     len(c.Response().Body()),
+			logging.ErrorMessage: "",
+		}
+
+		if err != nil {
+			keys[logging.ErrorMessage] = err.Error()
 		}
 
 		logger.Info(logging.RequestResponse, logging.API, "HTTP Request", keys)
