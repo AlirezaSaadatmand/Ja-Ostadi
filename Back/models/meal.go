846eda64abf120fa44ea32c80874e07b6715d28f
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-10-14 13:31:07 +0330

    add(docs): adding swagger docs for new route

--- Back/models/meal.go
+++ Back/models/meal.go
@@ -4,9 +4,14 @@ import "gorm.io/gorm"
 
 type Meal struct {
 	gorm.Model
-	DayID  uint
-	Type   string
-	Name   string
-	Price  string
-	Rating float64
+	DayID       uint
+	Type        string
+	Name        string
+	Price       string
+	Rating      float64
+	Discription string
+	Place       string
+
+	ImageID *uint
+	Image   *MealImage `gorm:"foreignKey:ImageID"`
 }
