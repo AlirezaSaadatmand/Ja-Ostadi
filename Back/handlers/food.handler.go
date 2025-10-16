21bb841f680de6a3fe60002b8fb052ca1befa767
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-10-16 19:05:04 +0330

    add(food.handler.go): add new handler for getting weekly meals

--- Back/handlers/food.handler.go
+++ Back/handlers/food.handler.go
@@ -3,10 +3,76 @@ package handlers
 import (
 	"path/filepath"
 	"strconv"
+	"strings"
 
+	"github.com/AlirezaSaadatmand/Ja-Ostadi/types"
 	"github.com/AlirezaSaadatmand/Ja-Ostadi/utils"
 	"github.com/gofiber/fiber/v2"
 )
+
+// GetWeeklyFood retrieves the current week's meal schedule
+// @Summary Get weekly food schedule
+// @Description Returns the most recent week's food plan, including all days and their respective breakfast, lunch, and dinner meals.
+// @Tags food
+// @Accept json
+// @Produce json
+// @Success 200 {object} utils.APIResponse{data=types.FoodData}
+// @Failure 500 {object} utils.APIResponse
+// @Router /food/weekly [get]
+func (h *Handler) GetWeeklyFood(c *fiber.Ctx) error {
+	var data types.FoodData
+
+	week, err := h.Services.GetLastWeekMeals()
+	if err != nil {
+		return utils.Error(c, fiber.StatusInternalServerError, err.Error())
+	}
+
+	data.Week = week.Week
+
+	days, err := h.Services.GetDaysByWeekID(int(week.ID))
+	if err != nil {
+		return utils.Error(c, fiber.StatusInternalServerError, err.Error())
+	}
+
+	for _, day := range days {
+		var dayFood types.DayFoodData
+		dayFood.Day = day.Day
+		dayFood.Date = day.Date
+
+		meals, err := h.Services.GetMealsByDayID(int(day.ID))
+		if err != nil {
+			return utils.Error(c, fiber.StatusInternalServerError, err.Error())
+		}
+
+		for _, meal := range meals {
+			m := types.MealData{
+				Name:        meal.Name,
+				Price:       meal.Price,
+				Rating:      float32(meal.Rating),
+				Description: meal.Description,
+				Place:       meal.Place,
+			}
+
+			// if meal.Image != nil {
+			// 	m.ImageAddress = meal.Image.URL // change field name if needed (e.g. `meal.Image.Address`)
+			// }
+
+			switch strings.ToLower(meal.Type) {
+			case "breakfast":
+				dayFood.Breakfast = append(dayFood.Breakfast, m)
+			case "lunch":
+				dayFood.Lunch = append(dayFood.Lunch, m)
+			case "dinner":
+				dayFood.Dinner = append(dayFood.Dinner, m)
+			}
+		}
+
+		data.Meals = append(data.Meals, dayFood)
+	}
+
+	return utils.Success(c, fiber.StatusOK, data, "Meal image uploaded successfully")
+
+}
 
 // UploadMealImage uploads an image and stores metadata
 // @Summary Upload a meal image
@@ -90,3 +156,5 @@ func (h *Handler) UpdateMealImage(c *fiber.Ctx) error {
 
 	return utils.Success(c, fiber.StatusOK, updatedMeal, "Meal image and/or keywords updated successfully")
 }
+
+
