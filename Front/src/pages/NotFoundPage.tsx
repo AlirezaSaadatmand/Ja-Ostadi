b9caf170e24c54f74a367a73b58403e49eb885cf
ParsaBordbar <parsab71@gmail.com>
2025-10-20 01:29:13 +0330

    refactor(pages/NotFound): easter-egg

--- Front/src/pages/NotFoundPage.tsx
+++ Front/src/pages/NotFoundPage.tsx
@@ -1,6 +1,15 @@
 import type React from "react"
 
+const notFoundMessages = [
+  'اخخخ رفتی تو دیوار که :)',
+  'بَد شد که :/',
+  'یا خودش میاد یا خبرش ... فعلا که خبری ازش نی',
+  'به کاه‌دون زدی مهندس',
+  'فععک نکنم این صفحه وجود داشته باشه'
+];
+
 const NotFoundPage: React.FC = () => {
+  const giveOneToMe = Math.floor(Math.random() * notFoundMessages.length);
   return (
     <div
       className="min-h-screen bg-gradient-to-br from-gray-50 to-white flex flex-col items-center justify-center p-8 font-sans text-center"
@@ -9,7 +18,7 @@ const NotFoundPage: React.FC = () => {
       <h1 className="text-[10rem] font-extrabold text-indigo-600 mb-8">۴۰۴</h1>
       <h2 className="text-4xl font-bold mb-4 text-gray-900">صفحه مورد نظر یافت نشد</h2>
       <p className="text-gray-700 text-base mb-8 max-w-md">
-       اخخخ رفتی تو دیوار که :)
+       {notFoundMessages[giveOneToMe]}
       </p>
       <a
         href="/"
