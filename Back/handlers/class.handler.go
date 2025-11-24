ed086f9792be88fcfe0dc381a2ff6fac8a34ca8f
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-11-24 18:09:49 +0330

    update(class.handler.go): rewrite the handler to use the new model

--- Back/handlers/class.handler.go
+++ Back/handlers/class.handler.go
@@ -8,15 +8,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func doesExists(class string, classes []string) bool {
-	for _, c := range classes {
-		if class == c {
-			return true
-		}
-	}
-	return false
-}
-
 // GetAllRooms returns a list of unique rooms from ClassTime
 // @Summary Get all rooms
 // @Description Returns all unique room names from the schedule
@@ -27,33 +18,13 @@ func doesExists(class string, classes []string) bool {
 // @Router /schedule/rooms [get]
 func (h *Handler) GetAllRooms(c *fiber.Ctx) error {
 
-	courses, err := h.Services.GetCoursesSchedule()
-	if err != nil {
-		return utils.Error(c, fiber.StatusInternalServerError, err.Error())
-	}
-
-	var courseId []uint
-	for _, course := range courses {
-		courseId = append(courseId, course.ID)
-	}
-
 	rooms, err := h.Services.GetAllUniqueRooms()
 	if err != nil {
 		return utils.Error(c, fiber.StatusInternalServerError, err.Error())
 	}
 
-	var thisSemesterRooms []string
 
-	for _, class := range rooms {
-		for _, id := range courseId {
-			if class.CourseID == id && !doesExists(class.Room, thisSemesterRooms) {
-				thisSemesterRooms = append(thisSemesterRooms, class.Room)
-				break
-			}
-		}
-	}
-
-	return utils.Success(c, fiber.StatusOK, thisSemesterRooms, "Rooms fetched successfully")
+	return utils.Success(c, fiber.StatusOK, rooms, "Rooms fetched successfully")
 }
 
 type RoomScheduleResponse struct {
